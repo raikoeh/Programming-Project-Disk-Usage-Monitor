cb8c48dde329e8c482080124c67f4175495d7d0d
Rachel Harris <rahharris@ucdavis.edu>
2026-03-04 14:33:42 -0800

    added script to loop over directories

--- disk_monitor.sh
+++ disk_monitor.sh
@@ -8,3 +8,14 @@ fi
 
 THRESHOLD="$1"
 TOTAL_SIZE=0
+
+# Loop over directories to measure size
+DIRECTORY=2 # start at 2 since the directory is the second argument, skips threshold argument
+while [ $DIRECTORY -le $# ]; do # while directory = or < number of arguments, DIR is current directory
+    DIR=${!DIRECTORY}
+
+    if [ ! -d "$DIR" ]; then # filter out non-directory inputs
+	echo "Error: $DIR is not a valid input, skipping $DIR"
+	DIRECTORY=$((DIRECTORY + 1))
+	continue # move onto next argument
+    fi 
