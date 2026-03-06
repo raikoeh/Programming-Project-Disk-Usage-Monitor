f6cc50b66f813190a4a3c29f27e4ff55a665813f
Rachel Harris <rahharris@ucdavis.edu>
2026-03-05 23:01:02 -0800

    Added/revised script for disk usage report and output

--- disk_monitor.sh
+++ disk_monitor.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# ========== Validate ==========
+# ========== Setup ==========
+
+THRESHOLD="$1"
+Total_size=0
+Warnings=0
+Checked=0
+DIRECTORY=2
+
 validate() {
   if [ "$#" -lt 2 ]; then
       echo "Usage: $0 <threshold_percent> <directory1> [directory2 ...]"
       exit 1
   fi
-
-  THRESHOLD="$1"
-  TOTAL_SIZE=0
-
-  if ! [[ "$THRESHOLD" =~ ^[0-9]+$ ]]; then
-    echo "Error: Threshold must be an integer."
-    exit 1
-  fi
   
-  DIRECTORY=2
   
   while [ "$DIRECTORY" -le "$#" ]; do # while directory = or < number of arguments, DIR is current directory
     DIR=${!DIRECTORY}
@@ -26,7 +24,6 @@ validate() {
         continue # move onto next argument
     fi 
 
-    echo "Valid directory: $DIR"
 
     DIRECTORY=$((DIRECTORY+1))
 
@@ -36,28 +33,40 @@ validate() {
 
 # ========== Disk Usage Analysis  ==========
 analysis() {
-  total_size=0
-  warnings=0
-  directories_checked=0
 
   echo "=== Disk Usage Report ==="
 
-  for DIR in "$@" # directories in all arguments
-      size_bytes=$(du -sb "$DIR" | cut -f1) # prints disk usage in bytes
-      size_human=$(du -sh "$DIR" | cut -f1) # prints disk usage in human readable
+  for DIR in "$@"
+  do # directories in all arguments
+
+    size_bytes=$(du -sb "$DIR" | cut -f1) # prints directory disk usage in bytes
+
+    size_human=$(du -sh "$DIR" | cut -f1) # prints directory disk usage in human readable
+
+    total=$(df -h "$DIR" | tail -1) # prints human readable disk filesystem size
+
+    percent=$((size_bytes * 100 / total)) # change later, tentative
+    
+    if [ "$percent" -ge "$THRESHOLD" ]; then
+	echo "$DIR     $size_human [WARINING: exceeds ${THRESHOLD}% threshold.]" # if percent exceeds threshold
+	Warnings=$((Warnings+1))
+    else
+	echo "$DIR     $size_human"
+    fi
+
+    Total_size=$((Total_size + size_bytes))
+    Checked=$((Checked + 1))
+
+  done
+
+  }
 
 
-      percent=$((size / total)) # change later, tentative
-}
-# fix/add to this portion
+# ========== Main Pipeline ==========
 
-# ========== Output ==========
+validate "$@"
+analysis "$@"
 
-if [ "$percentage" -ge "$THRESHOLD" ]; then
-    echo "$DIR     $size_human [WARINING: exceeds ${THRESHOLD}% threshold.]" # if percent exceeds threshold
-    warnings=$((warnings+1))
-
-else
-    echo "$DIR     $size_human" # if percent doesn't exceed threshold
-fi
-# revise if loop ^^
+echo "Total: $total"
+echo "Directories Checked: $Checked"
+echo "Warnings: $Warnings"
